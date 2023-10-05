1e408ee01bc63245b12055a66f2be5283588e4eb
Isa Sunasra <isa.rizwan@vit.edu.in>
2023-10-05 15:26:00 +0530

    Added client server communication

--- client/app.py
+++ client/app.py
@@ -1,61 +1,145 @@
-import requests
-import os
-import tensorflow as tf
-import pickle
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+import pickle
+import json
+import requests
+import os
 
-# URL of the server where the global model is hosted
-SERVER_URL = 'http://localhost:5000/model'  # Replace with the actual server IP and port
-
-# Directory to save the downloaded model
+SERVER_URL = 'http://localhost:5000'
 SAVE_DIR = 'global_model/'
 
-# Load the tokenizer
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-    print(tokenizer)
-
+    
 def download_global_model():
-    response = requests.get(SERVER_URL)
+    model_path = os.path.join(SAVE_DIR, 'model.h5')
 
-    # Ensure the save directory exists
+    if os.path.exists(model_path):
+        print('Global model already exists. Skipping download.')
+        return
+    
+    response = requests.get(SERVER_URL + "/model")
+
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
 
-    # Save the received model to the client's directory
     model_path = os.path.join(SAVE_DIR, 'model.h5')
     with open(model_path, 'wb') as model_file:
         model_file.write(response.content)
 
     print('Global model downloaded successfully.')
 
+download_global_model()
+tokenizer = pickle.load(open('tokenizer.pickle', 'rb'))
+model = tf.keras.models.load_model('global_model/model.h5')
 
-def load_global_model():
-    # Load the model
-    model = tf.keras.models.load_model(os.path.join(SAVE_DIR, 'model.h5'))
-    return model
+for layer in model.layers[:-1]:
+    layer.trainable = False
 
-def predict_next_word(seed_text, mode, tokenizer):
-    token_list = tokenizer.texts_to_sequences([seed_text])[0]
-    token_list = pad_sequences([token_list], maxlen=40-1, padding='pre')
-    predicted=model.predict(token_list)
-    predicted=np.argmax(predicted,axis=1)
-    output_word = ""
+app = Flask(__name__)
 
-    for word, index in tokenizer.word_index.items():
-        if index == predicted:
-            output_word = word
-            break
-    return output_word
+user_choices = []
+
+def predict_next_word(seed_text):
+    try:
+        predict_word = ""
+
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=6-1, padding='pre')
+        predicted = model.predict(token_list)
+        predicted = np.argmax(predicted, axis=1)
+
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                predict_word += word
+        return predict_word
+    
+    except Exception as ex:
+        print(ex)
+        return ""
+    
+def upload_updated_model():
+    global SERVER_URL
+
+    # Check if the model file exists
+    if os.path.exists("global_model/model.h5"):
+        # Upload the updated model to the server
+        with open("global_model/model.h5", 'rb') as model_file:
+            files = {'file': model_file}
+            response = requests.post(f'{SERVER_URL}/upload_model', files=files)
+
+        if response.status_code == 200:
+            print('Updated model uploaded successfully to the server.')
+        else:
+            print('Failed to upload the updated model to the server.')
+    else:
+        print('Updated model file not found. Please ensure the file path is correct.')
+    
+def fine_tune_model(fine_tuning_data):
+    try:
+        model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), metrics=['accuracy'])
+
+        input_sequences = []
+
+        for line in fine_tuning_data:
+            token_list = tokenizer.texts_to_sequences([line])[0];
+
+            for i in range(1, len(token_list)):
+                n_gram_sequence = token_list[:i+1]
+                input_sequences.append(n_gram_sequence)
+
+        if len(input_sequences) == 0:
+            return
+
+        total_words = len(tokenizer.word_index) + 1
+        max_sequence_len = 6
+        input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+        xs, labels = input_sequences[:,:-1],input_sequences[:,-1]
+        ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+        model.fit(xs, ys, epochs=1, verbose=1)
+        print(xs.shape, labels.shape, ys.shape, "------ Model recompiled")
+        model.save("global_model/model.h5")
+        upload_updated_model()
+
+    except Exception as ex:
+        print(ex)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        user_input = request.form['user_input']
+        predicted_word = predict_next_word(user_input)
+        return jsonify({'predicted_word': predicted_word})
+    except Exception as ex:
+        print(ex)
+
+
+
+@app.route('/store_user_choice', methods=['POST'])
+def store_user_choice():
+    global user_choices
+    try:
+        data = json.loads(request.data)
+        user_input = data['user_input']
+
+        fine_tune_model([user_input])
+
+        return jsonify({'message': 'User choice stored successfully'})
+    except Exception as ex:
+        print(ex)
+        return jsonify({'message': 'An error occured'})
+
 
 if __name__ == '__main__':
-    download_global_model()
-    model = load_global_model()
-
-    seed_texts = ["This is", "Can you", "How", "What about", "How are", "I do", "Bananas", "Apples", "I love"]
-    for seed_text in seed_texts:
-        predicted_word = predict_next_word(seed_text, model, tokenizer)
-        print(f'Next predicted word after "{seed_text}": {predicted_word}')
-
+    try:
+        app.run(port=5001)
+    except Exception as ex:
+        print(ex)
--- server/app.py
+++ server/app.py
@@ -2,16 +2,21 @@
 # the model is trained on a corpus of 1000 books from project Gutenberg
 
 import os
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify, render_template
+import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 app = Flask(__name__, static_url_path='')
 
 MODEL_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model')
-
+tokenizer = pickle.load(open('tokenizer.pickle', 'rb'))
+model = tf.keras.models.load_model(MODEL_FOLDER + '/model.h5')
 # make a get endpoint at  /, that just returns a string to know that the server is running
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return 'Server is running'
+    return render_template('index.html')
 
 @app.route('/model', methods=['GET'])
 def get_model():
@@ -21,5 +26,50 @@ def get_model():
         print(e)
         return jsonify({'error': 'model not found'}), 404
     
+@app.route('/upload_model', methods=['POST'])
+def upload_model():
+    global MODEL_FOLDER
+
+    # Check if the 'file' key is in the request files
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+
+    model_path = os.path.join(MODEL_FOLDER, 'model.h5')
+    file.save(model_path)
+
+    return jsonify({'message': 'Updated model uploaded successfully'}), 200
+
+def predict_next_word(seed_text):
+    try:
+        predict_word = ""
+
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=6-1, padding='pre')
+        predicted = model.predict(token_list)
+        predicted = np.argmax(predicted, axis=1)
+
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                predict_word += word
+        return predict_word
+    
+    except Exception as ex:
+        print(ex)
+        return ""
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        print(request.form)
+        user_input = request.form['user_input']
+        predicted_word = predict_next_word(user_input)
+        return jsonify({'predicted_word': predicted_word})
+    except Exception as ex:
+        print(ex)
+    
 if __name__ == '__main__':
-    app.run( port=5000, debug=True)
+    app.run( port=5000)
